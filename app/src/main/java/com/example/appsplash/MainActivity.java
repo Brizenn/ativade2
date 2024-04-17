896e8fae4c79cae9f72690bca3c2571353ee22f8
nathan <nathanvictor456@gmail.com>
2024-04-16 20:24:55 -0400

    Initial commit

---
+++ app/src/main/java/com/example/appsplash/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.appsplash;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        Thread thread1 = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    try {
+                        sleep(505);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    startActivity(new Intent(MainActivity.this, SplashActivy.class));
+                    finish();
+
+
+                } catch (RuntimeException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+
+        }; thread1.start();
+    }
+}
